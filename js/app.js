f6e7959a43de14e57d2c15c6cdb134cc45961948
Roseanna McFarlane <play.with.fashion@gmail.com>
2016-01-14 21:37:08 +1100

    added button callback

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 /*
+//step one. One Cat
 var i = 0;   //Global variable. Starting clicks at 0
 
 $('#cat-img').click(function() {//setting click function on cat img
@@ -18,9 +19,39 @@ var cats = [ //cat array object
         img: "img/tibbles.png",
         clicks: 0,
         id: "img-2"
+    },
+    {
+        name: "Peach",
+        img: "img/peach.png",
+        clicks: 0,
+        id: "img-3"
+    },
+    {
+        name: "Spots",
+        img: "img/spots.png",
+        clicks: 0,
+        id: "img-4"
+    },
+    {
+        name: "Fluffy",
+        img: "img/fluffy.png",
+        clicks: 0,
+        id: "img-5"
+    },
+    {
+        name: "Violet",
+        img: "img/violet.png",
+        clicks: 0,
+        id: "img-5"
     }
+
 ];
 
+/*var catimg = cats.slice(0,1); //trying to access img from cat object
+console.log(catimg);*/
+
+/*
+//Step 2, two cats
 function appendCats(cats){// cats is the array of objects
     cats.forEach(function(cat){// for each cat in the cat object. The function is called for EACH cat
         var catdiv = $('<div/>');
@@ -35,9 +66,9 @@ function appendCats(cats){// cats is the array of objects
         });
 
         catdiv.append(newimage);
-        /*newimage.src = cat.img;
+        /!*newimage.src = cat.img;
         newimage.id = cat.id;
-        newimage.className = ("cat");*/
+        newimage.className = ("cat");*!/
 
 
         var catclicks = $('<h2/>');
@@ -54,50 +85,56 @@ function appendCats(cats){// cats is the array of objects
     })
 }
 appendCats(cats);
+*/
 
-/*function appendCats(array) { //append the array to the page. Give the function the cats array as input
-    for (var i = 0; i < array.length; i++){ // keep appending objects until it is the length of the array
-        var catdiv = document.createElement('div');// create the div where the cat objects will go
+//Step 3, Three cats.
 
-        var cattitle = document.createElement('h3'); //create h3 tag
-        cattitle.innerHTML = array[i].name; //add cat name
-        catdiv.appendChild(cattitle); //append to the dom
+function appendCats(cats){
+    cats.forEach(function(cat){
+        /*var catdiv = $('<div/>');*/
+        var catlist = $('<li/>'); //create li element
+        var catbutton = $('<button/>');//create button element
+        var cattitle = $('<h3/>');//create h3 element
+        cattitle.text(cat.name); //append cat name to h3
+        catbutton.append(cattitle); //append h3 to button
+        catlist.append(catbutton); //append button to li
+        $("ul").append(catlist); //append li to ul
 
-        var newimage = document.createElement('img'); //create img tag
-        newimage.src = array[i].img; //add cat img
-        newimage.id = array[i].id;// add img id
-        newimage.className = ("cat"); //add cat class
-        catdiv.appendChild(newimage); //append to dom
+        //cat clicks
+        /*var catclicks = $('<h2/>');//cat click h2
+        catclicks.text(cat.clicks);//cat click text is the cat clicks
+        $("#catclicks").append(catclicks);*/
 
+        //images
+        var newimage = $('<img/>', {
+            src: cat.img,
+            id:  cat.id,
+            class: "cat"
+        });
 
-        var catclicks = document.createElement('h2'); //create h2 tag for clicks
-        catclicks.innerHTML = array[i].clicks; //append the clicks to h2
-        catdiv.appendChild(catclicks); //append to the dom
+        //buttons are linked to images
+        catbutton.click(function() {//button does two things. Puts image on the page and adds clicks
+            $("#emptyBox").empty();
+            $("#emptyBox").append(newimage)
+            var catclickheader = $('<h2/>');
+            catclickheader.text("Clicks");
+            $("#emptyBox").append(catclickheader);
+        });
 
-        document.body.appendChild(catdiv); //append the cat div with object properties
-        var elem = document.getElementsByTagName('img');
-
-    }
-        /!*$(".cat").click(function(){ //setting click function on cat images
-             //need to append clicks to each cats count
-        });*!/
-
-    for (var i = elem.length - 1; i >= 0; i--) {
-        elem[i].addEventListener("click",clickhandler,false);
-    }
-
-    function clickhandler(){
-
-        /// I want to use the item from the array. as such array[i].count =+ 1but it doesn't work
-
-        console.log();
-    }
-
+    })
 }
 
-appendCats(cats);*/
+appendCats(cats);
 
 
+/*
+$('button').click(function() {//button does two things. Puts image on the page and adds clicks
+    var newimage = $('<img/>', {
+        src: cats.img,
+        id:  cats.id,
+        class: "cat"
+    });
+    $("#emptyBox").append(newimage)
 
-
-
+});
+*/
